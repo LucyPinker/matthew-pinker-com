6867455497467f45b5b4d9d81d647a382f5f2db4
Lucy Pinker <lucy.pinker@sage.com>
2024-11-03 16:43:47 +0000

    changes to enable deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',    // This tells Next.js to export the app as a static site
+  trailingSlash: true, // Optional: Adds a trailing slash to the end of all routes
+};
 
 export default nextConfig;
